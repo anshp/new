769f96c70fd5588a4f3c675c5d3f99c12ceef896
anshp <anshuman@riktamtech.com>
2014-09-25 17:48:23 +0530

    deleted some files

--- nee.php
+++
@@ -1,5 +0,0 @@
-<?php
-$a = session_id();
-if(empty($a)) session_start();
-echo "SID: ".SID."<br>session_id(): ".session_id()."<br>COOKIE: ".$_COOKIE["PHPSESSID"];
-?>
--- new.php
+++
@@ -1,9 +0,0 @@
-<?php
-for ($i=0;$i<20;$i++){
-	$aa[$i]=$i;
-}
-
-echo array_search(2, $aa);
-echo array_search(4, $aa);   // $key = 1;
-
-?>
